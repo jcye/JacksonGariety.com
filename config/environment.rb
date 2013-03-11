aa75e5a861f8384f03a86f413c2edc0e477e6249
Jackson Gariety <personal@jacksongariety.com>
2013-03-10 21:30:23 -0700

    Fixes time zone.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 JacksongarietyDev::Application.initialize!
+
+# Timezone
+config.time_zone = "Pacific Time (US & Canada)"
